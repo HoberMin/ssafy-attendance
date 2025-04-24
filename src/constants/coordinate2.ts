e5fdcb56f2e33b5e6dc8ee13b54e3ccee019b1e1
HoberMin <102784200+HoberMin@users.noreply.github.com>
2025-04-24 09:48:53 +0900

    fix: 변경 요청서 폼 수정에 따른 좌표 수정

--- src/constants/coordinate2.ts
+++ src/constants/coordinate2.ts
@@ -1,34 +1,34 @@
 export const TEXT_COORD: Record<string, [number, number]> = {
-  campus: [0.333, 0.282],
-  classNumber: [0.56, 0.282],
-  name: [0.333, 0.326],
-  birthDate: [0.67, 0.325],
-  attendanceDateYear: [0.36, 0.56],
-  attendanceDateMonth: [0.5, 0.56],
-  attendanceDateDay: [0.6, 0.56],
-  attendanceTimeHour: [0.7, 0.56],
-  attendanceTimeMinute: [0.83, 0.56],
-  changeDateYear: [0.36, 0.593],
-  changeDateMonth: [0.5, 0.593],
-  changeDateDay: [0.6, 0.593],
-  changeTimeHour: [0.7, 0.593],
-  changeTimeMinute: [0.83, 0.593],
-  changeReason: [0.24, 0.65],
-  requestName: [0.34, 0.692],
+  campus: [0.35, 0.2625],
+  classNumber: [0.56, 0.262],
+  name: [0.35, 0.305],
+  birthDate: [0.67, 0.305],
+  attendanceDateYear: [0.36, 0.587],
+  attendanceDateMonth: [0.5, 0.587],
+  attendanceDateDay: [0.6, 0.587],
+  attendanceTimeHour: [0.7, 0.587],
+  attendanceTimeMinute: [0.83, 0.587],
+  changeDateYear: [0.36, 0.62],
+  changeDateMonth: [0.5, 0.62],
+  changeDateDay: [0.6, 0.62],
+  changeTimeHour: [0.7, 0.62],
+  changeTimeMinute: [0.83, 0.62],
+  changeReason: [0.24, 0.675],
+  requestName: [0.34, 0.715],
 };
 
 export const REASON_CHECK_COORD: [number, number][] = [
-  [0.22, 0.42],
-  [0.39, 0.42],
-  [0.56, 0.42],
-  [0.725, 0.42],
+  [0.22, 0.444],
+  [0.387, 0.444],
+  [0.56, 0.444],
+  [0.72, 0.444],
 ];
 
-export const SIGNATURE_COORD = [0.77, 0.66];
+export const SIGNATURE_COORD = [0.77, 0.672];
 
 // 제일 아래 제출일 텍스트 좌표
 export const DOCS_DATE_COORD: Record<string, [number, number]> = {
-  currentYear: [0.374, 0.897],
-  currentMonth: [0.48, 0.897],
-  currentDay: [0.58, 0.897],
+  currentYear: [0.378, 0.8925],
+  currentMonth: [0.48, 0.8925],
+  currentDay: [0.58, 0.8925],
 };
